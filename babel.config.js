81ff3a49900797840d5725988e14768e288bf204
Zachary Ebenfeld <zackebenfeld@gmail.com>
2026-05-21 01:16:49 -0400

    moved replit backgammon app into `replit` folder and created obytes app `npx create-obytes-app@latest BackgammonMastermind`

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = (api) => {
-	api.cache(true);
-	return {
-		presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-	};
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo',
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@env': './src/lib/env.js',
+          },
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,8 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
+const { withUniwindConfig } = require('uniwind/metro');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: './src/global.css',
+});
